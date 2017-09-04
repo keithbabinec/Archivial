8df1e56bfd7ed2f44bcd4b1a1f8c74f00710e181
Keith Babinec <keith.babinec@gmail.com>
2017-09-04 14:53:15 -0700

    Added empty projects for library, clients, and test

---
+++ OzetteClientAgent/OzetteClientAgent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OzetteClientAgent
+{
+    public partial class OzetteClientAgent : ServiceBase
+    {
+        public OzetteClientAgent()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
