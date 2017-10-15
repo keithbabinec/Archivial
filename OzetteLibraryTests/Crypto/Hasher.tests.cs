457963b9302158f49c95d3399dcc3be9a67a639c
Keith Babinec <keith.babinec@gmail.com>
2017-10-15 16:49:31 -0700

    Moved hasher test files into dedicated subfolder

--- OzetteLibraryTests/Crypto/Hasher.tests.cs
+++ OzetteLibraryTests/Crypto/Hasher.tests.cs
@@ -20,7 +20,7 @@ namespace OzetteLibraryTests.Crypto
         {
             OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
 
-            byte[] actualHash = h.Generate20ByteFileHash(".\\TestFiles\\SmallFile.txt");
+            byte[] actualHash = h.Generate20ByteFileHash(".\\TestFiles\\Hasher\\SmallFile.txt");
             byte[] expectedHash = { 39, 16, 25, 68, 128, 64, 131, 150, 216, 25,
                                     111, 227, 135, 18, 122, 14, 163, 85, 226, 30 };
 
@@ -39,7 +39,7 @@ namespace OzetteLibraryTests.Crypto
         {
             OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
 
-            byte[] actualHash = h.Generate32ByteFileHash(".\\TestFiles\\SmallFile.txt");
+            byte[] actualHash = h.Generate32ByteFileHash(".\\TestFiles\\Hasher\\SmallFile.txt");
             byte[] expectedHash = { 105, 105, 114, 199, 90, 53, 52, 53, 173, 45,
                                     254, 222, 255, 128, 156, 96, 21, 79, 196, 31,
                                     4, 129, 237, 45, 93, 69, 246, 90, 61, 209, 80, 64 };
@@ -59,7 +59,7 @@ namespace OzetteLibraryTests.Crypto
         {
             OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
 
-            byte[] actualHash = h.Generate64ByteFileHash(".\\TestFiles\\SmallFile.txt");
+            byte[] actualHash = h.Generate64ByteFileHash(".\\TestFiles\\Hasher\\SmallFile.txt");
             byte[] expectedHash = { 30, 175, 186, 169, 203, 212, 173, 146, 227, 20, 65,
                                     15, 115, 42, 115, 68, 95, 1, 236, 65, 180, 175, 251,
                                     135, 40, 244, 205, 140, 242, 222, 6, 7, 140, 4, 179,
@@ -82,7 +82,7 @@ namespace OzetteLibraryTests.Crypto
         {
             OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
 
-            byte[] actualHash = h.Generate20ByteFileHash(".\\TestFiles\\MediumFile.mp3");
+            byte[] actualHash = h.Generate20ByteFileHash(".\\TestFiles\\Hasher\\MediumFile.mp3");
             byte[] expectedHash = { 59, 46, 56, 72, 141, 74, 31, 120, 17, 24, 52,
                                     218, 140, 133, 179, 49, 83, 159, 149, 50 };
 
@@ -101,7 +101,7 @@ namespace OzetteLibraryTests.Crypto
         {
             OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
 
-            byte[] actualHash = h.Generate32ByteFileHash(".\\TestFiles\\MediumFile.mp3");
+            byte[] actualHash = h.Generate32ByteFileHash(".\\TestFiles\\Hasher\\MediumFile.mp3");
             byte[] expectedHash = { 59, 223, 174, 134, 63, 70, 226, 183, 168, 86, 148,
                                     117, 40, 147, 139, 169, 231, 2, 41, 38, 8, 109, 55,
                                     221, 0, 10, 29, 173, 245, 93, 11, 59 };
@@ -121,7 +121,7 @@ namespace OzetteLibraryTests.Crypto
         {
             OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
 
-            byte[] actualHash = h.Generate64ByteFileHash(".\\TestFiles\\MediumFile.mp3");
+            byte[] actualHash = h.Generate64ByteFileHash(".\\TestFiles\\Hasher\\MediumFile.mp3");
             byte[] expectedHash = { 234, 71, 228, 136, 179, 247, 148, 228, 21, 49, 132, 155,
                                     219, 239, 173, 165, 249, 35, 144, 199, 224, 127, 103,
                                     207, 211, 205, 191, 43, 207, 232, 154, 191, 238, 13, 128,
