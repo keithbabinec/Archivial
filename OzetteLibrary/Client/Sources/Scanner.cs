844fc216e169706a743c9ecc2caafba1488c42e6
Keith Babinec <keith.babinec@gmail.com>
2017-10-22 17:47:39 -0700

    Implemented scanner constructor, BeginScan() and StopScan()

--- OzetteLibrary/Client/Sources/Scanner.cs
+++ OzetteLibrary/Client/Sources/Scanner.cs
@@ -1,6 +1,7 @@
 ﻿using OzetteLibrary.Database;
 using OzetteLibrary.Models;
 using System;
+using System.Threading;
 
 namespace OzetteLibrary.Client.Sources
 {
@@ -15,7 +16,17 @@ namespace OzetteLibrary.Client.Sources
         /// <param name="source"></param>
         public Scanner(SourceLocation source, IDatabase database)
         {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (database == null)
+            {
+                throw new ArgumentNullException(nameof(database));
+            }
+
+            Source = source;
+            Database = database;
         }
 
         /// <summary>
@@ -23,7 +34,19 @@ namespace OzetteLibrary.Client.Sources
         /// </summary>
         public void BeginScan()
         {
-            throw new NotImplementedException();
+            lock (ScanStatusLock)
+            {
+                if (ScanInProgress)
+                {
+                    throw new InvalidOperationException("Cannot start the scan. It is already in progress.");
+                }
+
+                ScanInProgress = true;
+                ScanStopRequested = false;
+            }
+
+            Thread scanThread = new Thread(() => Scan());
+            scanThread.Start();
         }
 
         /// <summary>
@@ -31,12 +54,51 @@ namespace OzetteLibrary.Client.Sources
         /// </summary>
         public void StopScan()
         {
-            throw new NotImplementedException();
+            lock (ScanStatusLock)
+            {
+                if (ScanInProgress)
+                {
+                    ScanStopRequested = true;
+                }
+            }
         }
 
         /// <summary>
         /// Event triggered when the scan has completed.
         /// </summary>
         public event EventHandler<ScanResults> ScanCompleted;
+
+        /// <summary>
+        /// A reference to the database.
+        /// </summary>
+        private IDatabase Database { get; set; }
+
+        /// <summary>
+        /// A reference to the Source details.
+        /// </summary>
+        private SourceLocation Source { get; set; }
+
+        /// <summary>
+        /// Flag to indicate if the scan is already in progress.
+        /// </summary>
+        private bool ScanInProgress { get; set; }
+
+        /// <summary>
+        /// Flag to indicate if the scan stop has been requested.
+        /// </summary>
+        private bool ScanStopRequested { get; set; }
+
+        /// <summary>
+        /// Thread locking mechanism.
+        /// </summary>
+        private object ScanStatusLock { get; set; }
+
+        /// <summary>
+        /// Performs a scan of the source location.
+        /// </summary>
+        private void Scan()
+        {
+
+        }
     }
 }
