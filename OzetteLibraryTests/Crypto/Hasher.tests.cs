cc43e1e1865f77287fd8a255aa19706d275fe592
Keith Babinec <keith.babinec@gmail.com>
2017-10-28 16:32:26 -0700

    Added unit tests for hasher.TwoHashesAreTheSame()

--- OzetteLibraryTests/Crypto/Hasher.tests.cs
+++ OzetteLibraryTests/Crypto/Hasher.tests.cs
@@ -15,6 +15,146 @@ namespace OzetteLibraryTests.Crypto
             Assert.AreEqual("39-16-25-68-128-64-55-27", h.HashBytesToString(bytes));
         }
 
+        [TestMethod]
+        public void HasherCompareReturnsTrueForSameHash1()
+        {
+            OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
+
+            byte[] bytes1 = { 39, 16, 25, 68, 128, 64, 55, 27 };
+            byte[] bytes2 = { 39, 16, 25, 68, 128, 64, 55, 27 };
+
+            Assert.IsTrue(h.TwoHashesAreTheSame(bytes1, bytes2));
+        }
+
+        [TestMethod]
+        public void HasherCompareReturnsTrueForSameHash2()
+        {
+            OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
+
+            byte[] bytes1 = { 39, 16, 25, 68, 128, 64, 55, 27 };
+            byte[] bytes2 = { 39, 16, 25, 68, 128, 64, 55, 27 };
+
+            Assert.IsTrue(h.TwoHashesAreTheSame(bytes2, bytes1));
+        }
+
+        [TestMethod]
+        public void HasherCompareReturnsTrueForSameHash3()
+        {
+            OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
+
+            byte[] bytes1 = { 39, 16, 25, 68, 128, 64, 131, 150, 216, 25,
+                                    111, 227, 135, 18, 122, 14, 163, 85, 226, 30 };
+            byte[] bytes2 = { 39, 16, 25, 68, 128, 64, 131, 150, 216, 25,
+                                    111, 227, 135, 18, 122, 14, 163, 85, 226, 30 };
+
+            Assert.IsTrue(h.TwoHashesAreTheSame(bytes1, bytes2));
+        }
+
+        [TestMethod]
+        public void HasherCompareReturnsTrueForSameHash4()
+        {
+            OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
+
+            byte[] bytes1 = { 234, 71, 228, 136, 179, 247, 148, 228, 21, 49, 132, 155,
+                                    219, 239, 173, 165, 249, 35, 144, 199, 224, 127, 103,
+                                    207, 211, 205, 191, 43, 207, 232, 154, 191, 238, 13, 128,
+                                    221, 58, 116, 68, 175, 197, 121, 110, 243, 35, 53, 228,
+                                    214, 206, 251, 250, 253, 35, 149, 27, 127, 233, 201, 36,
+                                    219, 107, 45, 136, 215 };
+            byte[] bytes2 = { 234, 71, 228, 136, 179, 247, 148, 228, 21, 49, 132, 155,
+                                    219, 239, 173, 165, 249, 35, 144, 199, 224, 127, 103,
+                                    207, 211, 205, 191, 43, 207, 232, 154, 191, 238, 13, 128,
+                                    221, 58, 116, 68, 175, 197, 121, 110, 243, 35, 53, 228,
+                                    214, 206, 251, 250, 253, 35, 149, 27, 127, 233, 201, 36,
+                                    219, 107, 45, 136, 215 };
+
+            Assert.IsTrue(h.TwoHashesAreTheSame(bytes1, bytes2));
+        }
+
+        [TestMethod]
+        public void HasherCompareReturnsFalseForDifferentHashes1()
+        {
+            OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
+
+            byte[] bytes1 = { 235, 71, 228, 136, 179, 247, 148, 228, 21, 49, 132, 155,
+                                    219, 239, 173, 165, 249, 35, 144, 199, 224, 127, 103,
+                                    207, 211, 205, 191, 43, 207, 232, 154, 191, 238, 13, 128,
+                                    221, 58, 116, 68, 175, 197, 121, 110, 243, 35, 53, 228,
+                                    214, 206, 251, 250, 253, 35, 149, 27, 127, 233, 201, 36,
+                                    219, 107, 45, 136, 215 };
+            byte[] bytes2 = { 234, 71, 228, 136, 179, 247, 148, 228, 21, 49, 132, 155,
+                                    219, 239, 173, 165, 249, 35, 144, 199, 224, 127, 103,
+                                    207, 211, 205, 191, 43, 207, 232, 154, 191, 238, 13, 128,
+                                    221, 58, 116, 68, 175, 197, 121, 110, 243, 35, 53, 228,
+                                    214, 206, 251, 250, 253, 35, 149, 27, 127, 233, 201, 36,
+                                    219, 107, 45, 136, 215 };
+
+            Assert.IsFalse(h.TwoHashesAreTheSame(bytes1, bytes2));
+        }
+
+        [TestMethod]
+        public void HasherCompareReturnsFalseForDifferentHashes2()
+        {
+            OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
+
+            byte[] bytes1 = { 234, 71, 228, 136, 179, 247, 148, 228, 21, 49, 132, 155,
+                                    219, 239, 173, 165, 249, 35, 144, 199, 224, 127, 103,
+                                    207, 211, 205, 191, 43, 208, 232, 154, 191, 238, 13, 128,
+                                    221, 58, 116, 68, 175, 197, 121, 110, 243, 35, 53, 228,
+                                    214, 206, 251, 250, 253, 35, 149, 27, 127, 233, 201, 36,
+                                    219, 107, 45, 136, 215 };
+            byte[] bytes2 = { 234, 71, 228, 136, 179, 247, 148, 228, 21, 49, 132, 155,
+                                    219, 239, 173, 165, 249, 35, 144, 199, 224, 127, 103,
+                                    207, 211, 205, 191, 43, 207, 232, 154, 191, 238, 13, 128,
+                                    221, 58, 116, 68, 175, 197, 121, 110, 243, 35, 53, 228,
+                                    214, 206, 251, 250, 253, 35, 149, 27, 127, 233, 201, 36,
+                                    219, 107, 45, 136, 215 };
+
+            Assert.IsFalse(h.TwoHashesAreTheSame(bytes1, bytes2));
+        }
+
+        [TestMethod]
+        public void HasherCompareReturnsFalseForDifferentHashes3()
+        {
+            OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
+
+            byte[] bytes1 = { 234, 71, 228, 136, 179, 247, 148, 228, 21, 49, 132, 155,
+                                    219, 239, 173, 165, 249, 35, 144, 199, 224, 127, 103,
+                                    207, 211, 205, 191, 43, 207, 232, 154, 191, 238, 13, 128,
+                                    221, 58, 116, 68, 175, 197, 121, 110, 243, 35, 53, 228,
+                                    214, 206, 251, 250, 253, 35, 149, 27, 127, 233, 201, 36,
+                                    219, 107, 45, 136, 215 };
+            byte[] bytes2 = { 234, 71, 228, 136, 179, 247, 148, 228, 21, 49, 132, 155,
+                                    219, 239, 173, 165, 249, 35, 144, 199, 224, 127, 103,
+                                    207, 211, 205, 191, 43, 207, 232, 154, 191, 238, 13, 128,
+                                    221, 58, 116, 68, 175, 197, 121, 110, 243, 35, 53, 228,
+                                    214, 206, 251, 250, 253, 35, 149, 27, 127, 233, 201, 36,
+                                    219, 107, 45, 136 };
+
+            Assert.IsFalse(h.TwoHashesAreTheSame(bytes1, bytes2));
+        }
+
+        [TestMethod]
+        public void HasherCompareReturnsFalseForDifferentHashes4()
+        {
+            OzetteLibrary.Crypto.Hasher h = new OzetteLibrary.Crypto.Hasher();
+
+            byte[] bytes1 = { 234, 71, 228, 136, 179, 247, 148, 228, 21, 49, 132, 155,
+                                    219, 239, 173, 165, 249, 35, 144, 199, 224, 127, 103,
+                                    207, 211, 205, 191, 43, 207, 232, 154, 191, 238, 13, 128,
+                                    221, 58, 116, 68, 175, 197, 121, 110, 243, 35, 53, 228,
+                                    214, 206, 251, 250, 253, 35, 149, 27, 127, 233, 201, 36,
+                                    219, 107, 45, 136, 215 };
+            byte[] bytes2 = { 234, 71, 228, 136, 179, 247, 148, 228, 21, 49, 132, 155,
+                                    219, 239, 173, 165, 249, 35, 144, 199, 224, 127, 103,
+                                    207, 211, 205, 191, 43, 207, 232, 154, 191, 238, 13, 128,
+                                    221, 58, 116, 68, 175, 197, 121, 110, 243, 35, 53, 228,
+                                    214, 206, 251, 250, 253, 35, 149, 27, 127, 233, 201, 36,
+                                    219, 107, 45, 136, 216 };
+
+            Assert.IsFalse(h.TwoHashesAreTheSame(bytes1, bytes2));
+        }
+
         [TestMethod()]
         public void HasherCanCorrectlyGenerate20ByteSmallFileHash()
         {
@@ -24,14 +164,7 @@ namespace OzetteLibraryTests.Crypto
             byte[] expectedHash = { 39, 16, 25, 68, 128, 64, 131, 150, 216, 25,
                                     111, 227, 135, 18, 122, 14, 163, 85, 226, 30 };
 
-            Assert.AreEqual(expectedHash.Length, actualHash.Length);
-
-            for (int i = 0; i < expectedHash.Length; i++)
-            {
-                Assert.AreEqual(expectedHash[i], actualHash[i], 
-                    string.Format("Byte index {0} is incorrect. Encountered: {1}. Expected {2}", 
-                    i, h.HashBytesToString(actualHash), h.HashBytesToString(expectedHash)));
-            }
+            Assert.IsTrue(h.TwoHashesAreTheSame(actualHash, expectedHash));
         }
 
         [TestMethod()]
@@ -44,14 +177,7 @@ namespace OzetteLibraryTests.Crypto
                                     254, 222, 255, 128, 156, 96, 21, 79, 196, 31,
                                     4, 129, 237, 45, 93, 69, 246, 90, 61, 209, 80, 64 };
 
-            Assert.AreEqual(expectedHash.Length, actualHash.Length);
-
-            for (int i = 0; i < expectedHash.Length; i++)
-            {
-                Assert.AreEqual(expectedHash[i], actualHash[i], 
-                    string.Format("Byte index {0} is incorrect. Encountered: {1}. Expected {2}", 
-                    i, h.HashBytesToString(actualHash), h.HashBytesToString(expectedHash)));
-            }
+            Assert.IsTrue(h.TwoHashesAreTheSame(actualHash, expectedHash));
         }
 
         [TestMethod()]
@@ -67,14 +193,7 @@ namespace OzetteLibraryTests.Crypto
                                     5, 201, 129, 225, 249, 132, 172, 224, 172, 225, 206,
                                     164, 213, 166, 106, 49, 170 };
 
-            Assert.AreEqual(expectedHash.Length, actualHash.Length);
-
-            for (int i = 0; i < expectedHash.Length; i++)
-            {
-                Assert.AreEqual(expectedHash[i], actualHash[i], 
-                    string.Format("Byte index {0} is incorrect. Encountered: {1}. Expected {2}", 
-                    i, h.HashBytesToString(actualHash), h.HashBytesToString(expectedHash)));
-            }
+            Assert.IsTrue(h.TwoHashesAreTheSame(actualHash, expectedHash));
         }
 
         [TestMethod()]
@@ -86,14 +205,7 @@ namespace OzetteLibraryTests.Crypto
             byte[] expectedHash = { 59, 46, 56, 72, 141, 74, 31, 120, 17, 24, 52,
                                     218, 140, 133, 179, 49, 83, 159, 149, 50 };
 
-            Assert.AreEqual(expectedHash.Length, actualHash.Length);
-
-            for (int i = 0; i < expectedHash.Length; i++)
-            {
-                Assert.AreEqual(expectedHash[i], actualHash[i],
-                    string.Format("Byte index {0} is incorrect. Encountered: {1}. Expected {2}",
-                    i, h.HashBytesToString(actualHash), h.HashBytesToString(expectedHash)));
-            }
+            Assert.IsTrue(h.TwoHashesAreTheSame(actualHash, expectedHash));
         }
 
         [TestMethod()]
@@ -106,14 +218,7 @@ namespace OzetteLibraryTests.Crypto
                                     117, 40, 147, 139, 169, 231, 2, 41, 38, 8, 109, 55,
                                     221, 0, 10, 29, 173, 245, 93, 11, 59 };
 
-            Assert.AreEqual(expectedHash.Length, actualHash.Length);
-
-            for (int i = 0; i < expectedHash.Length; i++)
-            {
-                Assert.AreEqual(expectedHash[i], actualHash[i],
-                    string.Format("Byte index {0} is incorrect. Encountered: {1}. Expected {2}",
-                    i, h.HashBytesToString(actualHash), h.HashBytesToString(expectedHash)));
-            }
+            Assert.IsTrue(h.TwoHashesAreTheSame(actualHash, expectedHash));
         }
 
         [TestMethod()]
@@ -129,14 +234,7 @@ namespace OzetteLibraryTests.Crypto
                                     214, 206, 251, 250, 253, 35, 149, 27, 127, 233, 201, 36,
                                     219, 107, 45, 136, 215 };
 
-            Assert.AreEqual(expectedHash.Length, actualHash.Length);
-
-            for (int i = 0; i < expectedHash.Length; i++)
-            {
-                Assert.AreEqual(expectedHash[i], actualHash[i],
-                    string.Format("Byte index {0} is incorrect. Encountered: {1}. Expected {2}",
-                    i, h.HashBytesToString(actualHash), h.HashBytesToString(expectedHash)));
-            }
+            Assert.IsTrue(h.TwoHashesAreTheSame(actualHash, expectedHash));
         }
     }
 }
