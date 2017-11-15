26d5894bf884834005b7727b7720ade110dbe742
Keith Babinec <keith.babinec@gmail.com>
2017-11-14 22:21:48 -0800

    Added new client file index lookup model/enum

---
+++ OzetteLibrary/Models/ClientFileLookup.cs
@@ -0,0 +1,18 @@
+﻿namespace OzetteLibrary.Models
+{
+    /// <summary>
+    /// Contains the result of a Client file lookup.
+    /// </summary>
+    public class ClientFileLookup
+    {
+        /// <summary>
+        /// The resulting client file, if found, otherwise null.
+        /// </summary>
+        public ClientFile File { get; set; }
+
+        /// <summary>
+        /// The result of the lookup operation.
+        /// </summary>
+        public ClientFileLookupResult Result { get; set; }
+    }
+}
---
+++ OzetteLibrary/Models/ClientFileLookupResult.cs
@@ -0,0 +1,58 @@
+﻿namespace OzetteLibrary.Models
+{
+    /// <summary>
+    /// Describes the possible results of a file lookup attempt.
+    /// </summary>
+    /// <remarks>
+    /// This result is returned when looking up a file in the database index.
+    /// The lookup included 3-factors: file name, file location (directory), and file hash.
+    /// Depending on how these factors have changed or not changed, results in one of the listed states below.
+    /// </remarks>
+    public enum ClientFileLookupResult
+    {
+        /// <summary>
+        /// The file is new to the database.
+        /// </summary>
+        /// <remarks>
+        /// Lookup combination:
+        /// > { Filename mismatch, Location mismatch, Hash mismatch }
+        /// </remarks>
+        New = 0,
+
+        /// <summary>
+        /// This file is already indexed.
+        /// </summary>
+        /// <remarks>
+        /// Lookup combination:
+        /// > { Filename matched, Location matched, Hash matched }
+        /// </remarks>
+        Existing = 1,
+
+        /// <summary>
+        /// The file is in the same place, but has been updated.
+        /// </summary>
+        /// <remarks>
+        /// Lookup combination:
+        /// > { Filename matched, Location matched, Hash mismatch }
+        /// </remarks>
+        Updated = 2,
+
+        /// <summary>
+        /// File has been renamed.
+        /// </summary>
+        /// <remarks>
+        /// Lookup combination:
+        /// > { Filename mismatch, Location matched, Hash matched }
+        /// </remarks>
+        Renamed = 3,
+
+        /// <summary>
+        /// File has moved/copied to a new location.
+        /// </summary>
+        /// <remarks>
+        /// Lookup combination:
+        /// > { Filename matched, Location mismatch, Hash matched }
+        /// </remarks>
+        Moved = 4
+    }
+}
