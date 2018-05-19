8a9567802864774fc889f78b8f2e141027660018
Keith Babinec <keith.babinec@gmail.com>
2018-05-19 12:45:39 -0700

    Removed TargetDB implementation and tests

--- OzetteLibrary/Database/ITargetDatabase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OzetteLibrary.Database
-{
-    /// <summary>
-    /// A generic database interface for the target database.
-    /// </summary>
-    public interface ITargetDatabase : IDatabase
-    {
-    }
-}
