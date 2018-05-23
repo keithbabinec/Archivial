5f53b11618d68ebebc9354b8e5299a33a939e449
Keith Babinec <keith.babinec@gmail.com>
2018-05-22 23:29:55 -0700

    Added unit tests for ProviderFileStatus

---
+++ OzetteLibraryTests/Providers/ProviderFileStatus.tests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OzetteLibraryTests.Providers
+{
+    [TestClass]
+    public class ProviderFileStatusTests
+    {
+        [TestMethod()]
+        public void ProviderFileStatusResetStateDoesNotThrowIfNoProviderIsPresent()
+        {
+            var copyState = new OzetteLibrary.Providers.ProviderFileStatus();
+            copyState.ResetState();
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void ProviderFileStatusCorrectlyAssignsProviderFromConstructor()
+        {
+            var copyState = new OzetteLibrary.Providers.ProviderFileStatus(OzetteLibrary.Providers.ProviderTypes.AWS);
+
+            Assert.AreEqual(OzetteLibrary.Providers.ProviderTypes.AWS, copyState.Provider);
+        }
+
+        [TestMethod()]
+        public void ProviderFileStatusCorrectlyInitializesCopyStateAsUnsynced()
+        {
+            var copyState = new OzetteLibrary.Providers.ProviderFileStatus(OzetteLibrary.Providers.ProviderTypes.AWS);
+
+            Assert.AreEqual(-1, copyState.LastCompletedFileBlockIndex);
+            Assert.AreEqual(OzetteLibrary.Files.FileStatus.Unsynced, copyState.SyncStatus);
+        }
+
+        [TestMethod()]
+        public void ProviderFileStatusResetStateCorrectlyResetsProgress()
+        {
+            var copyState = new OzetteLibrary.Providers.ProviderFileStatus(OzetteLibrary.Providers.ProviderTypes.AWS);
+            copyState.LastCompletedFileBlockIndex = 100;
+            copyState.SyncStatus = OzetteLibrary.Files.FileStatus.InProgress;
+            copyState.ResetState();
+
+            Assert.AreEqual(OzetteLibrary.Providers.ProviderTypes.AWS, copyState.Provider);
+            Assert.AreEqual(-1, copyState.LastCompletedFileBlockIndex);
+            Assert.AreEqual(OzetteLibrary.Files.FileStatus.Unsynced, copyState.SyncStatus);
+        }
+
+        [TestMethod()]
+        public void ProviderFileStatusResetStateCorrectlyResetsProgress2()
+        {
+            var copyState = new OzetteLibrary.Providers.ProviderFileStatus(OzetteLibrary.Providers.ProviderTypes.AWS);
+            copyState.LastCompletedFileBlockIndex = 100;
+            copyState.SyncStatus = OzetteLibrary.Files.FileStatus.Synced;
+            copyState.ResetState();
+
+            Assert.AreEqual(OzetteLibrary.Providers.ProviderTypes.AWS, copyState.Provider);
+            Assert.AreEqual(-1, copyState.LastCompletedFileBlockIndex);
+            Assert.AreEqual(OzetteLibrary.Files.FileStatus.Unsynced, copyState.SyncStatus);
+        }
+    }
+}
