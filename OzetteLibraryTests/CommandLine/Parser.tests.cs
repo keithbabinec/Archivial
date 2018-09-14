f51a28078414fa62cd8c68468661891264d5c63d
Keith Babinec <keith.babinec@gmail.com>
2018-09-14 15:14:06 -0700

    Added unit tests for azure and encryption command parsers

--- OzetteLibraryTests/CommandLine/Parser.tests.cs
+++ OzetteLibraryTests/CommandLine/Parser.tests.cs
@@ -118,5 +118,98 @@ namespace OzetteLibraryTests.CommandLine
             Assert.AreEqual("C:\\path", installArgs.InstallDirectory);
             Assert.AreEqual("C:\\path\\to\\my.db", installArgs.DatabasePath);
         }
+
+        [TestMethod]
+        public void ParserCanParseConfigureAzureCommandWithBothArgs()
+        {
+            string[] arguments = { "configure-azure", "--azurestorageaccountname", "myaccount", "--azurestorageaccounttoken", "mytoken" };
+            Arguments parsed;
+
+            var parser = new Parser();
+
+            Assert.IsTrue(parser.Parse(arguments, out parsed));
+            Assert.IsInstanceOfType(parsed, typeof(ConfigureAzureArguments));
+
+            var installArgs = parsed as ConfigureAzureArguments;
+
+            Assert.AreEqual("myaccount", installArgs.AzureStorageAccountName);
+            Assert.AreEqual("mytoken", installArgs.AzureStorageAccountToken);
+        }
+
+        [TestMethod]
+        public void ParserReturnsFalseWhenConfigureAzureHasNoArgsPassed()
+        {
+            string[] arguments = { "configure-azure" };
+            Arguments parsed;
+
+            var parser = new Parser();
+
+            Assert.IsFalse(parser.Parse(arguments, out parsed));
+            Assert.IsNull(parsed);
+        }
+
+        [TestMethod]
+        public void ParserReturnsFalseWhenConfigureAzureIsMissingStorageAccount()
+        {
+            string[] arguments = { "configure-azure", "--azurestorageaccounttoken", "mytoken" };
+            Arguments parsed;
+
+            var parser = new Parser();
+
+            Assert.IsFalse(parser.Parse(arguments, out parsed));
+            Assert.IsNull(parsed);
+        }
+
+        [TestMethod]
+        public void ParserReturnsFalseWhenConfigureAzureIsMissingStorageToken()
+        {
+            string[] arguments = { "configure-azure", "--azurestorageaccountname", "myaccount" };
+            Arguments parsed;
+
+            var parser = new Parser();
+
+            Assert.IsFalse(parser.Parse(arguments, out parsed));
+            Assert.IsNull(parsed);
+        }
+
+        [TestMethod]
+        public void ParserCanParseConfigureEncryptionCommandWithValidArgs()
+        {
+            string[] arguments = { "configure-encryption", "--protectioniv", "mykey" };
+            Arguments parsed;
+
+            var parser = new Parser();
+
+            Assert.IsTrue(parser.Parse(arguments, out parsed));
+            Assert.IsInstanceOfType(parsed, typeof(ConfigureEncryptionArguments));
+
+            var installArgs = parsed as ConfigureEncryptionArguments;
+
+            Assert.AreEqual("mykey", installArgs.ProtectionIv);
+        }
+
+        [TestMethod]
+        public void ParserReturnsFalseWhenConfigureEncryptionHasNoArgsPassed()
+        {
+            string[] arguments = { "configure-encryption" };
+            Arguments parsed;
+
+            var parser = new Parser();
+
+            Assert.IsFalse(parser.Parse(arguments, out parsed));
+            Assert.IsNull(parsed);
+        }
+
+        [TestMethod]
+        public void ParserReturnsFalseWhenConfigureEncryptionWhenWrongOptionProvided()
+        {
+            string[] arguments = { "configure-encryption", "--wrongoption", "mykey" };
+            Arguments parsed;
+
+            var parser = new Parser();
+
+            Assert.IsFalse(parser.Parse(arguments, out parsed));
+            Assert.IsNull(parsed);
+        }
     }
 }
