8fa9c479a50da6bec009a2da410a6f2d66d57e77
Keith Babinec <keith.babinec@gmail.com>
2018-09-15 17:04:23 -0700

    Created setup step stubs

--- OzetteLibrary/CommandLine/Commands/Install.cs
+++ OzetteLibrary/CommandLine/Commands/Install.cs
@@ -1,4 +1,5 @@
-﻿using OzetteLibrary.Logging.Default;
+﻿using System;
+using OzetteLibrary.Logging.Default;
 using OzetteLibrary.ServiceCore;
 
 namespace OzetteLibrary.CommandLine.Commands
@@ -17,11 +18,70 @@ namespace OzetteLibrary.CommandLine.Commands
             var logger = new Logger(Constants.Logging.InstallationComponentName);
             logger.WriteConsole("Starting Ozette Cloud Backup installation");
 
-            logger.WriteConsole("Appling core settings.");
-            SetCoreSettings(arguments);
+            logger.WriteConsole("Applying core settings.");
+            CreateCoreSettings(arguments);
+
+            logger.WriteConsole("Setting up installation directories.");
+            CreateInstallationDirectories();
+
+            logger.WriteConsole("Copying installation files.");
+            CopyProgramFiles();
+
+            logger.WriteConsole("Creating initial database.");
+            CreateInitialDatabase();
+
+            logger.WriteConsole("Creating Ozette Client Service.");
+            CreateClientService();
+
+            logger.WriteConsole("Starting Ozette Client Service.");
+            StartClientService();
         }
 
-        private static void SetCoreSettings(InstallationArguments arguments)
+        /// <summary>
+        /// Creates the installation directories.
+        /// </summary>
+        private static void CreateInstallationDirectories()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Copies the program files to the installation directory.
+        /// </summary>
+        private static void CopyProgramFiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates the initial database.
+        /// </summary>
+        private static void CreateInitialDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates the client windows service.
+        /// </summary>
+        private static void CreateClientService()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Starts the client windows service.
+        /// </summary>
+        private static void StartClientService()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sets the core application settings.
+        /// </summary>
+        /// <param name="arguments"></param>
+        private static void CreateCoreSettings(InstallationArguments arguments)
         {
             // set the core settings.
 
