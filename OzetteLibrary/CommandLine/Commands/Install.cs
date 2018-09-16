c53ac7d4da0e448a65fc0b737b8ff2931da42d25
keithbabinec <keith.babinec@gmail.com>
2018-09-16 14:29:49 -0700

    Renamed event log source name to fit windows naming length restriction

--- OzetteLibrary/CommandLine/Commands/Install.cs
+++ OzetteLibrary/CommandLine/Commands/Install.cs
@@ -70,7 +70,7 @@ namespace OzetteLibrary.CommandLine.Commands
 
             CoreSettings.InstallationDirectory = arguments.InstallDirectory;
             CoreSettings.LogFilesDirectory = Path.Combine(arguments.InstallDirectory, "Logs");
-            CoreSettings.EventlogName = "OzetteCloudBackup";
+            CoreSettings.EventlogName = "Ozette";
 
             var dbPath = Path.Combine(arguments.InstallDirectory, "Database\\OzetteCloudBackup.db");
             CoreSettings.DatabaseConnectionString = string.Format("Filename={0};Journal=true;Mode=Shared", dbPath);
