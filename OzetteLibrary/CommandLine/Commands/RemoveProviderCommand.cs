865560619026fd959835cf912faf93aa391e4313
keithbabinec <keith.babinec@gmail.com>
2018-09-26 22:38:24 -0700

    Implemented command.run() for remove-provider and remove-source

--- OzetteLibrary/CommandLine/Commands/RemoveProviderCommand.cs
+++ OzetteLibrary/CommandLine/Commands/RemoveProviderCommand.cs
@@ -1,7 +1,10 @@
 ﻿using OzetteLibrary.CommandLine.Arguments;
+using OzetteLibrary.Database.LiteDB;
 using OzetteLibrary.Logging.Default;
+using OzetteLibrary.ServiceCore;
 using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace OzetteLibrary.CommandLine.Commands
 {
@@ -45,6 +48,7 @@ namespace OzetteLibrary.CommandLine.Commands
                 Logger.WriteConsole("--- Starting Ozette Cloud Backup provider configuration");
 
                 Logger.WriteConsole("--- Step 1: Remove the provider from the database.");
+                RemoveProvider(removeProviderArgs);
 
                 Logger.WriteConsole("--- Provider configuration completed successfully.");
 
@@ -56,6 +60,48 @@ namespace OzetteLibrary.CommandLine.Commands
                 Logger.WriteConsole(ex.ToString(), EventLogEntryType.Error);
                 return false;
             }
+        }
+
+        /// <summary>
+        /// Removes the specified provider.
+        /// </summary>
+        /// <param name="arguments"></param>
+        private void RemoveProvider(RemoveProviderArguments arguments)
+        {
+            Logger.WriteConsole("Initializing a database connection.");
+
+            var db = new LiteDBClientDatabase(CoreSettings.DatabaseConnectionString);
+            db.PrepareDatabase();
+
+            Logger.WriteConsole("Querying for existing cloud providers to see if the specified provider exists.");
+
+            var allProviders = db.GetProvidersList();
+            var providerToRemove = allProviders.FirstOrDefault(x => x.ID == arguments.ProviderID);
+
+            if (providerToRemove == null)
+            {
+                // the source doesn't exist. nothing to do.
+                Logger.WriteConsole("No cloud provider was found with the specified ID. Nothing to remove.");
+                return;
+            }
+
+            Logger.WriteConsole("Found a matching cloud provider, removing it now.");
+
+            allProviders.Remove(providerToRemove);
+            db.SetProviders(allProviders);
+
+            if (providerToRemove.Type == Providers.ProviderTypes.Azure)
+            {
+                // remove provider specific secrets
+                db.RemoveApplicationOption(Constants.OptionIDs.AzureStorageAccountName);
+                db.RemoveApplicationOption(Constants.OptionIDs.AzureStorageAccountToken);
+            }
+            else
+            {
+                throw new NotImplementedException("unexpected provider type.");
+            }
+
+            Logger.WriteConsole("Successfully removed the cloud provider from the database.");
         }
     }
 }
--- OzetteLibrary/CommandLine/Commands/RemoveSourceCommand.cs
+++ OzetteLibrary/CommandLine/Commands/RemoveSourceCommand.cs
@@ -1,7 +1,10 @@
 ﻿using OzetteLibrary.CommandLine.Arguments;
+using OzetteLibrary.Database.LiteDB;
 using OzetteLibrary.Logging.Default;
+using OzetteLibrary.ServiceCore;
 using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace OzetteLibrary.CommandLine.Commands
 {
@@ -45,6 +48,7 @@ namespace OzetteLibrary.CommandLine.Commands
                 Logger.WriteConsole("--- Starting Ozette Cloud Backup source configuration");
 
                 Logger.WriteConsole("--- Step 1: Remove the source from the database.");
+                RemoveSource(removeSrcArgs);
 
                 Logger.WriteConsole("--- Source configuration completed successfully.");
 
@@ -56,6 +60,37 @@ namespace OzetteLibrary.CommandLine.Commands
                 Logger.WriteConsole(ex.ToString(), EventLogEntryType.Error);
                 return false;
             }
+        }
+
+        /// <summary>
+        /// Removes the specified source.
+        /// </summary>
+        /// <param name="arguments"></param>
+        private void RemoveSource(RemoveSourceArguments arguments)
+        {
+            Logger.WriteConsole("Initializing a database connection.");
+
+            var db = new LiteDBClientDatabase(CoreSettings.DatabaseConnectionString);
+            db.PrepareDatabase();
+
+            Logger.WriteConsole("Querying for existing scan sources to see if the specified source exists.");
+
+            var allSources = db.GetAllSourceLocations();
+            var sourceToRemove = allSources.FirstOrDefault(x => x.ID == arguments.SourceID);
+
+            if (sourceToRemove == null)
+            {
+                // the source doesn't exist. nothing to do.
+                Logger.WriteConsole("No source was found with the specified ID. Nothing to remove.");
+                return;
+            }
+
+            Logger.WriteConsole("Found a matching backup source, removing it now.");
+
+            allSources.Remove(sourceToRemove);
+            db.SetSourceLocations(allSources);
+
+            Logger.WriteConsole("Successfully removed the source from the database.");
         }
     }
 }
