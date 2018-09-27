0e92ac2b205335cc18c8a243ca50194320528e38
keithbabinec <keith.babinec@gmail.com>
2018-09-26 22:15:42 -0700

    Added command line parser support for remove-provider and remove-source

--- OzetteLibrary/CommandLine/Parser.cs
+++ OzetteLibrary/CommandLine/Parser.cs
@@ -58,6 +58,14 @@ namespace OzetteLibrary.CommandLine
                 parsed = new ListProvidersArguments();
                 return true;
             }
+            else if (baseCommand == "remove-source")
+            {
+                return ParseRemoveSourceArgs(args, out parsed);
+            }
+            else if (baseCommand == "remove-provider")
+            {
+                return ParseRemoveProviderArgs(args, out parsed);
+            }
             else
             {
                 // unexpected/no base command provided.
@@ -206,6 +214,84 @@ namespace OzetteLibrary.CommandLine
             }
 
             parsed = sourceArgs;
+            return true;
+        }
+
+        /// <summary>
+        /// Parses the provided arguments into an <c>RemoveSourceArguments</c> object.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <param name="parsed"></param>
+        /// <returns></returns>
+        private bool ParseRemoveSourceArgs(string[] args, out ArgumentBase parsed)
+        {
+            // initialize args object with default
+            var remSrcArgs = new RemoveSourceArguments();
+            var map = ExtractArguments(args);
+
+            if (map.ContainsKey("sourceid"))
+            {
+                var sourceId = map["sourceid"];
+                int parsedId;
+
+                if (int.TryParse(sourceId, out parsedId))
+                {
+                    remSrcArgs.SourceID = parsedId;
+                }
+                else
+                {
+                    // required argument was not valid.
+                    parsed = null;
+                    return false;
+                }
+            }
+            else
+            {
+                // required argument was not found.
+                parsed = null;
+                return false;
+            }
+
+            parsed = remSrcArgs;
+            return true;
+        }
+
+        /// <summary>
+        /// Parses the provided arguments into an <c>RemoveProviderArguments</c> object.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <param name="parsed"></param>
+        /// <returns></returns>
+        private bool ParseRemoveProviderArgs(string[] args, out ArgumentBase parsed)
+        {
+            // initialize args object with default
+            var remSrcArgs = new RemoveProviderArguments();
+            var map = ExtractArguments(args);
+
+            if (map.ContainsKey("providerid"))
+            {
+                var providerId = map["providerid"];
+                int parsedId;
+
+                if (int.TryParse(providerId, out parsedId))
+                {
+                    remSrcArgs.ProviderID = parsedId;
+                }
+                else
+                {
+                    // required argument was not valid.
+                    parsed = null;
+                    return false;
+                }
+            }
+            else
+            {
+                // required argument was not found.
+                parsed = null;
+                return false;
+            }
+
+            parsed = remSrcArgs;
             return true;
         }
 
