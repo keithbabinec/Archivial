17b1aa20bc91c1bb2d8f9ffdaa6013feb219165a
keithbabinec <keith.babinec@gmail.com>
2018-10-02 23:51:29 -0700

    Updated provider metadata to include product name

--- OzetteLibrary/Constants/ProviderMetadata.cs
+++ OzetteLibrary/Constants/ProviderMetadata.cs
@@ -8,26 +8,26 @@
         /// <summary>
         /// The name of the metadata key for provider sync status.
         /// </summary>
-        public const string ProviderSyncStatusKeyName = "SyncStatus";
+        public const string ProviderSyncStatusKeyName = "OzetteSyncStatus";
 
         /// <summary>
         /// The name of the metadata key for last completed file block index.
         /// </summary>
-        public const string ProviderLastCompletedFileBlockIndexKeyName = "LastCompletedFileBlockIndex";
+        public const string ProviderLastCompletedFileBlockIndexKeyName = "OzetteLastCompletedFileBlockIndex";
 
         /// <summary>
         /// The name of the metadata key for the file's source path.
         /// </summary>
-        public const string FullSourcePathKeyName = "FileSourcePath";
+        public const string FullSourcePathKeyName = "OzetteFileSourcePath";
 
         /// <summary>
         /// The name of the metadata key for the file's computed hash.
         /// </summary>
-        public const string FileHash = "FileHash";
+        public const string FileHash = "OzetteFileHash";
 
         /// <summary>
         /// The name of the metadata key for the file's hash algorithm.
         /// </summary>
-        public const string FileHashAlgorithm = "FileHashAlgorithm";
+        public const string FileHashAlgorithm = "OzetteFileHashAlgorithm";
     }
 }
