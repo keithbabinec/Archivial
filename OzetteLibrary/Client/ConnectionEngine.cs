d19e09562ffb640fc3b55389eea363072a6fb05c
keithbabinec <keith.babinec@gmail.com>
2018-11-11 19:00:26 -0800

    Implemented core functions in the ConnectionEngine for managing net sources

--- OzetteLibrary/Client/ConnectionEngine.cs
+++ OzetteLibrary/Client/ConnectionEngine.cs
@@ -1,9 +1,15 @@
 ﻿using OzetteLibrary.Database;
 using OzetteLibrary.Engine;
 using OzetteLibrary.Events;
+using OzetteLibrary.Folders;
 using OzetteLibrary.Logging;
 using OzetteLibrary.Providers;
+using OzetteLibrary.Secrets;
+using OzetteLibrary.ServiceCore;
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Threading;
 
 namespace OzetteLibrary.Client
@@ -63,20 +69,19 @@ namespace OzetteLibrary.Client
                 while (true)
                 {
                     // query source locations from the database.
-
-                        // NetworkLocation 
-                        //  .IsConnected -- bool property
-                        //  .IsFailed -- bool property
-                        //  .LastConnectionCheck -- datetime property
-
-                    // do we have any network source connections that must be established or re-established?
-                    
-                        // 1. if a connection is not connected, attempt to connect it.
-                        // 2. if a connection is connected, but old, validate it.
-                        //      -- if working, then leave alone.
-                        //      -- if not working, then force disconnect (let it re-connect automatically on next cycle)
-
-                        // 3. take into account the timeout/penalty box for consistently failed connections.
+                    var sources = SafeGetSources();
+                    if (sources != null && sources.Count > 0)
+                    {
+                        foreach (var source in sources)
+                        {
+                            // if we have a network source, validate it.
+                            // ensure it is connected or re-established as needed.
+                            if (source is NetworkSourceLocation)
+                            {
+                                SafeVerifyNetSourceConnectionState(source as NetworkSourceLocation);
+                            }
+                        }
+                    }
 
                     ThreadSleepWithStopRequestCheck(TimeSpan.FromSeconds(60));
 
@@ -90,6 +95,214 @@ namespace OzetteLibrary.Client
             catch (Exception ex)
             {
                 OnStopped(new EngineStoppedEventArgs(ex));
+            }
+        }
+
+        /// <summary>
+        /// Grabs the source locations stored in the database.
+        /// </summary>
+        /// <remarks>
+        /// This function is marked as safe and should not throw exceptions.
+        /// </remarks>
+        /// <returns></returns>
+        private SourceLocations SafeGetSources()
+        {
+            try
+            {
+                var clientDB = Database as IClientDatabase;
+                return clientDB.GetAllSourceLocations();
+            }
+            catch (Exception ex)
+            {
+                string err = "Failed to read source locations from the database.";
+                Logger.WriteSystemEvent(err, ex, Logger.GenerateFullContextStackTrace(), Constants.EventIDs.FailedToLoadScanSources, true);
+
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Safely verifies a network source connection state.
+        /// </summary>
+        /// <remarks>
+        /// This function is marked as safe and should not throw exceptions.
+        /// </remarks>
+        /// <param name="netSource"></param>
+        /// <returns></returns>
+        private void SafeVerifyNetSourceConnectionState(NetworkSourceLocation netSource)
+        {
+            var dir = new DirectoryInfo(netSource.Path);
+            if (dir.Exists)
+            {
+                // we are already connected or have access.
+                // this is either an unauthenticated location or we have already establish a connection through net use.
+                UpdateNetSourceConnectionState(netSource, true, false);
+                return;
+            }
+
+            // force disconnect and reconnect
+
+            Logger.WriteTraceMessage("Attempting to connect to network source location: " + netSource.Path);
+
+            string netUser = null;
+            string netPass = null;
+
+            if (!string.IsNullOrWhiteSpace(netSource.CredentialName))
+            {
+                if (!TryGetNetSourceCredentials(netSource.CredentialName, out netUser, out netPass))
+                {
+                    Logger.WriteTraceError(string.Format(
+                        "Unable to connect to network source location: {0}. The specified credential ({1}) was not found in the stored credentials.", 
+                        netSource.Path, netSource.CredentialName));
+
+                    UpdateNetSourceConnectionState(netSource, false, true);
+                    return;
+                }
+            }
+
+            DisconnectNetUse(netSource);
+            ConnectNetUse(netSource, netUser, netPass);
+
+            // validate again: do we have access now after disconnect/reconnect?
+            // if we dont have access after running net use, then we can consider this connection failed.
+
+            var freshDir = new DirectoryInfo(netSource.Path);
+            if (freshDir.Exists)
+            {
+                UpdateNetSourceConnectionState(netSource, true, false);
+            }
+            else
+            {
+                UpdateNetSourceConnectionState(netSource, false, true);
+            }
+        }
+
+        /// <summary>
+        /// Disconnects a NET USE session.
+        /// </summary>
+        /// <param name="netSource"></param>
+        private void DisconnectNetUse(NetworkSourceLocation netSource)
+        {
+            var process = new Process();
+            var startInfo = new ProcessStartInfo()
+            {
+                FileName = "NET",
+                Arguments = string.Format("USE /DELETE {0}", netSource.Path),
+                RedirectStandardError = true
+            };
+
+            process.StartInfo = startInfo;
+
+            process.Start();
+            process.WaitForExit();
+
+            var stdErr = process.StandardError.ReadToEnd();
+
+            if (process.ExitCode != 0 && !string.IsNullOrWhiteSpace(stdErr))
+            {
+                string err = string.Format("Failed to disconnect a network source location: {0}. An error occurred calling NET USE. Error: {1}", netSource.Path, stdErr);
+                Logger.WriteTraceError(err);
+            }
+        }
+
+        /// <summary>
+        /// Connects a NET USE session.
+        /// </summary>
+        /// <param name="netSource"></param>
+        /// <param name="netPass"></param>
+        /// <param name="netUser"></param>
+        private void ConnectNetUse(NetworkSourceLocation netSource, string netUser, string netPass)
+        {
+            bool authenticated = !string.IsNullOrWhiteSpace(netUser) && !string.IsNullOrWhiteSpace(netPass);
+
+            var process = new Process();
+            var startInfo = new ProcessStartInfo()
+            {
+                FileName = "NET",
+                Arguments = authenticated ? 
+                    string.Format("USE {0} /USER:{1} {2}", netSource.Path, netUser, netPass) :
+                    string.Format("USE {0}", netSource.Path),
+                RedirectStandardError = true
+            };
+
+            process.StartInfo = startInfo;
+
+            process.Start();
+            process.WaitForExit();
+
+            var stdErr = process.StandardError.ReadToEnd();
+
+            if (process.ExitCode != 0 && !string.IsNullOrWhiteSpace(stdErr))
+            {
+                string err = string.Format("Failed to connect a network source location: {0}. An error occurred calling NET USE. Error: {1}", netSource.Path, stdErr);
+                Logger.WriteTraceError(err);
+            }
+        }
+
+        /// <summary>
+        /// Tries to get the network source location credentials by name, from the database.
+        /// </summary>
+        /// <param name="credentialName"></param>
+        /// <param name="netUser"></param>
+        /// <param name="netPass"></param>
+        /// <returns></returns>
+        private bool TryGetNetSourceCredentials(string credentialName, out string netUser, out string netPass)
+        {
+            var clientDB = Database as IClientDatabase;
+            var dbCredNames = clientDB.GetNetCredentialsList();
+
+            var foundCred = dbCredNames.FirstOrDefault(x => string.Equals(x.CredentialName, credentialName, StringComparison.CurrentCultureIgnoreCase));
+
+            if (foundCred == null)
+            {
+                netUser = null;
+                netPass = null;
+                return false;
+            }
+
+            try
+            {
+                var scope = System.Security.Cryptography.DataProtectionScope.LocalMachine;
+                var ivkey = Convert.FromBase64String(CoreSettings.ProtectionIv);
+                var pds = new ProtectedDataStore(clientDB, scope, ivkey);
+
+                netUser = pds.GetApplicationSecret(string.Format(Constants.Formats.NetCredentialUserNameKeyLookup, foundCred.CredentialName));
+                netPass = pds.GetApplicationSecret(string.Format(Constants.Formats.NetCredentialUserPasswordKeyLookup, foundCred.CredentialName));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string err = string.Format("Failed to lookup network credentials in the database: {0}. An error occurred: {1}", credentialName, ex.ToString());
+                Logger.WriteTraceError(err);
+
+                netUser = null;
+                netPass = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Updates the network source connection state in the database.
+        /// </summary>
+        /// <param name="netSource"></param>
+        /// <param name="isConnected"></param>
+        /// <param name="isFailed"></param>
+        private void UpdateNetSourceConnectionState(NetworkSourceLocation netSource, bool isConnected, bool isFailed)
+        {
+            try
+            {
+                var clientDB = Database as IClientDatabase;
+
+                netSource.IsConnected = isConnected;
+                netSource.IsFailed = isFailed;
+                netSource.LastConnectionCheck = DateTime.Now;
+
+                clientDB.UpdateSourceLocation(netSource);
+            }
+            catch (Exception ex)
+            {
+                string err = "Failed to save source locations to the database.";
+                Logger.WriteSystemEvent(err, ex, Logger.GenerateFullContextStackTrace(), Constants.EventIDs.FailedToSaveScanSources, true);
             }
         }
     }
