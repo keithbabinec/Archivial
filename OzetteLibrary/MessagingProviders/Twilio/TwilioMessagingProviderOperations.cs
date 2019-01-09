36fba479aaa95682869cd80fb4fe0a14ca0438e3
Keith Babinec <keith.babinec@gmail.com>
2019-01-08 21:39:38 -0800

    Bugfix: updated incorrect progress message formatting

--- OzetteLibrary/MessagingProviders/Twilio/TwilioMessagingProviderOperations.cs
+++ OzetteLibrary/MessagingProviders/Twilio/TwilioMessagingProviderOperations.cs
@@ -89,8 +89,8 @@ namespace OzetteLibrary.MessagingProviders.Twilio
 
             var format = new StringBuilder();
             format.AppendLine("Ozette Backup Status {0} {1}"); // <MachineName> <Overall Percent Completed>
-            format.AppendLine("Completed {2} {3}"); // <Files Completed> <File Size Completed>
-            format.AppendLine("Remaining {4} {5}"); // <Files Remaining> <File Size Remaining>
+            format.AppendLine("Completed {2} / {3}"); // <Files Completed> <File Size Completed>
+            format.AppendLine("Remaining {4} / {5}"); // <Files Remaining> <File Size Remaining>
 
             MessageFormat = format.ToString();
         }
@@ -102,7 +102,7 @@ namespace OzetteLibrary.MessagingProviders.Twilio
         public async Task SendBackupProgressStatusMessageAsync(BackupProgress Progress)
         {
             var finalMessage = string.Format(MessageFormat,
-                Environment.MachineName, Progress.OverallPercentage,
+                Environment.MachineName.ToUpper(), Progress.OverallPercentage,
                 Progress.BackedUpFileCount, Progress.BackedUpFileSize,
                 Progress.RemainingFileCount, Progress.RemainingFileCount);
 
