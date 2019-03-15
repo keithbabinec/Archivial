7508fc3970213d16754fb01749879921caa78a0a
Keith Babinec <keith.babinec@gmail.com>
2019-03-14 23:03:18 -0700

    Added scheduler implementation w/ unit tests

--- OzetteLibrary/Database/DatabaseBackupScheduler.cs
+++ OzetteLibrary/Database/DatabaseBackupScheduler.cs
@@ -10,11 +10,46 @@ namespace OzetteLibrary.Database
         /// <summary>
         /// Determines which backup can be performed, if any, based on the recent backup status.
         /// </summary>
+        /// <remarks>
+        /// Backup rules:
+        /// - Full Backup every 24 hours.
+        /// - Diff Backup every 4 hours.
+        /// - Log Backup every 30 minutes.
+        /// </remarks>
         /// <param name="RecentBackups"></param>
         /// <returns></returns>
         public DatabaseBackupType? NextDatabaseBackup(DatabaseBackupStatus RecentBackups)
         {
-            throw new NotImplementedException();
+            // When a backup is performed, all backups in lower tiers are flagged as completed for that timestamp.
+            // Completing a full backup flags Full, Diff, and Log backups as completed at the same time.
+            // Completing a diff backup flags the Diff and Log backups as completed at that time.
+            // This 'resets' the clock to make for an simpler algorithm.
+
+            if (RecentBackups.LastFullBackup == null 
+                || RecentBackups.LastDifferentialBackup == null
+                || RecentBackups.LastTransactionLogBackup == null)
+            {
+                return DatabaseBackupType.Full;
+            }
+
+            var now = DateTime.Now;
+
+            if (RecentBackups.LastFullBackup.Value >= now.AddHours(24))
+            {
+                return DatabaseBackupType.Full;
+            }
+
+            if (RecentBackups.LastDifferentialBackup.Value >= now.AddHours(4))
+            {
+                return DatabaseBackupType.Differential;
+            }
+
+            if (RecentBackups.LastTransactionLogBackup.Value >= now.AddMinutes(30))
+            {
+                return DatabaseBackupType.TransactionLog;
+            }
+
+            return null;
         }
     }
 }
---
+++ OzetteLibraryTests/Database/DatabaseBackupScheduler.tests.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OzetteLibrary.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OzetteLibraryTests.Database
+{
+    [TestClass]
+    public class DatabaseBackupSchedulerTests
+    {
+        DatabaseBackupScheduler Scheduler { get; set; }
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            Scheduler = new DatabaseBackupScheduler();
+        }
+
+        [TestMethod]
+        public void NextDatabaseBackupCorrectlyReturnsFullBackupIfNoBackupsTaken()
+        {
+            var recentBackups = new DatabaseBackupStatus()
+            {
+                LastFullBackup = null,
+                LastDifferentialBackup = null,
+                LastTransactionLogBackup = null
+            };
+
+            var result = Scheduler.NextDatabaseBackup(recentBackups);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(DatabaseBackupType.Full, result.Value);
+        }
+
+        [TestMethod]
+        public void NextDatabaseBackupCorrectlyReturnsNoBackupWhenNoBackupsNeedToBeTaken()
+        {
+            var now = DateTime.Now;
+
+            var recentBackups = new DatabaseBackupStatus()
+            {
+                LastFullBackup = now.AddHours(23),
+                LastDifferentialBackup = now.AddHours(3),
+                LastTransactionLogBackup = now.AddMinutes(29)
+            };
+
+            var result = Scheduler.NextDatabaseBackup(recentBackups);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void NextDatabaseBackupCorrectlyReturnsFullBackupIfNoFullBackupIn24Hours()
+        {
+            var now = DateTime.Now;
+
+            var recentBackups = new DatabaseBackupStatus()
+            {
+                LastFullBackup = now.AddHours(24),
+                LastDifferentialBackup = now.AddHours(2),
+                LastTransactionLogBackup = now.AddMinutes(5)
+            };
+
+            var result = Scheduler.NextDatabaseBackup(recentBackups);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(DatabaseBackupType.Full, result.Value);
+        }
+
+        [TestMethod]
+        public void NextDatabaseBackupCorrectlyReturnsDifferentialBackupIfNoDiffBackupsIn4Hours()
+        {
+            var now = DateTime.Now;
+
+            var recentBackups = new DatabaseBackupStatus()
+            {
+                LastFullBackup = now.AddHours(22),
+                LastDifferentialBackup = now.AddHours(4),
+                LastTransactionLogBackup = now.AddMinutes(5)
+            };
+
+            var result = Scheduler.NextDatabaseBackup(recentBackups);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(DatabaseBackupType.Differential, result.Value);
+        }
+
+        [TestMethod]
+        public void NextDatabaseBackupCorrectlyReturnsLogBackupIfNoLogBackupsIn30Minutes()
+        {
+            var now = DateTime.Now;
+
+            var recentBackups = new DatabaseBackupStatus()
+            {
+                LastFullBackup = now.AddHours(20),
+                LastDifferentialBackup = now.AddHours(3),
+                LastTransactionLogBackup = now.AddMinutes(30)
+            };
+
+            var result = Scheduler.NextDatabaseBackup(recentBackups);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(DatabaseBackupType.TransactionLog, result.Value);
+        }
+
+        [TestMethod]
+        public void NextDatabaseBackupCorrectlyPrioritizesLastFullOverLastDifferential()
+        {
+            var now = DateTime.Now;
+
+            var recentBackups = new DatabaseBackupStatus()
+            {
+                LastFullBackup = now.AddHours(25),
+                LastDifferentialBackup = now.AddHours(5),
+                LastTransactionLogBackup = now.AddMinutes(35)
+            };
+
+            var result = Scheduler.NextDatabaseBackup(recentBackups);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(DatabaseBackupType.Full, result.Value);
+        }
+
+        [TestMethod]
+        public void NextDatabaseBackupCorrectlyPrioritizesLastDifferentialOverLastTransactionLog()
+        {
+            var now = DateTime.Now;
+
+            var recentBackups = new DatabaseBackupStatus()
+            {
+                LastFullBackup = now.AddHours(20),
+                LastDifferentialBackup = now.AddHours(5),
+                LastTransactionLogBackup = now.AddMinutes(35)
+            };
+
+            var result = Scheduler.NextDatabaseBackup(recentBackups);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(DatabaseBackupType.Differential, result.Value);
+        }
+    }
+}
