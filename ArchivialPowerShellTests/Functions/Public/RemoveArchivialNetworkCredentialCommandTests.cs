42c12d1ee8d9065054a7c40610e905ee438d4b2d
Keith Babinec <keith.babinec@gmail.com>
2019-05-03 22:03:28 -0700

    Added PowerShell unit tests for remove provider and remove net creds commands.

---
+++ ArchivialPowerShellTests/Functions/Public/RemoveArchivialNetworkCredentialCommandTests.cs
@@ -0,0 +1,145 @@
+﻿using ArchivialLibrary.Database;
+using ArchivialLibrary.Secrets;
+using ArchivialPowerShell.Functions.Public;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchivialPowerShellTests.Functions.Public
+{
+    [TestClass]
+    public class RemoveArchivialNetworkCredentialCommandTests
+    {
+        [TestMethod]
+        public void RemoveArchivialNetworkCredentialCommand_NetCredentialParameter_HasRequiredAttributes()
+        {
+            Assert.IsTrue(
+                TypeHelpers.CmdletParameterHasAttribute(
+                    typeof(RemoveArchivialNetworkCredentialCommand),
+                    nameof(RemoveArchivialNetworkCredentialCommand.NetCredential),
+                    typeof(ParameterAttribute))
+            );
+
+            Assert.IsTrue(
+                TypeHelpers.CmdletParameterHasAttribute(
+                    typeof(RemoveArchivialNetworkCredentialCommand),
+                    nameof(RemoveArchivialNetworkCredentialCommand.NetCredential),
+                    typeof(ValidateNotNullAttribute))
+            );
+        }
+
+        [TestMethod]
+        public void RemoveArchivialNetworkCredentialCommand_CredentialNameParameter_HasRequiredAttributes()
+        {
+            Assert.IsTrue(
+                TypeHelpers.CmdletParameterHasAttribute(
+                    typeof(RemoveArchivialNetworkCredentialCommand),
+                    nameof(RemoveArchivialNetworkCredentialCommand.CredentialName),
+                    typeof(ParameterAttribute))
+            );
+
+            Assert.IsTrue(
+                TypeHelpers.CmdletParameterHasAttribute(
+                    typeof(RemoveArchivialNetworkCredentialCommand),
+                    nameof(RemoveArchivialNetworkCredentialCommand.CredentialName),
+                    typeof(ValidateNotNullOrEmptyAttribute))
+            );
+        }
+
+        [TestMethod]
+        public void RemoveArchivialNetworkCredentialCommand_CanRemoveCredential_FromCredentialName()
+        {
+            var mockedDb = new Mock<IClientDatabase>();
+
+            string databaseSentCredentialName = null;
+
+            mockedDb.Setup(x => x.GetNetCredentialsAsync()).ReturnsAsync(
+                new NetCredentialsCollection()
+                {
+                    new NetCredential() { CredentialName = "Test" }
+                }
+            );
+
+            mockedDb.Setup(x => x.RemoveNetCredentialAsync(It.IsAny<string>()))
+                .Returns(Task.CompletedTask)
+                .Callback<string>(x => databaseSentCredentialName = x);
+
+            var command = new RemoveArchivialNetworkCredentialCommand(mockedDb.Object)
+            {
+                CredentialName = "Test"
+            };
+
+            var result = command.Invoke().GetEnumerator().MoveNext();
+
+            mockedDb.Verify(x => x.RemoveNetCredentialAsync(It.IsAny<string>()), Times.Once);
+            mockedDb.Verify(x => x.RemoveApplicationOptionAsync(It.IsAny<string>()), Times.Exactly(2));
+
+            Assert.IsNotNull(databaseSentCredentialName);
+            Assert.AreEqual("Test", databaseSentCredentialName);
+        }
+
+        [TestMethod]
+        public void RemoveArchivialNetworkCredentialCommand_CanRemoveSource_FromSourceObject()
+        {
+            var mockedDb = new Mock<IClientDatabase>();
+
+            string databaseSentCredentialName = null;
+
+            mockedDb.Setup(x => x.GetNetCredentialsAsync()).ReturnsAsync(
+                new NetCredentialsCollection()
+                {
+                    new NetCredential() { CredentialName = "Test" }
+                }
+            );
+
+            mockedDb.Setup(x => x.RemoveNetCredentialAsync(It.IsAny<string>()))
+                .Returns(Task.CompletedTask)
+                .Callback<string>(x => databaseSentCredentialName = x);
+
+            var command = new RemoveArchivialNetworkCredentialCommand(mockedDb.Object)
+            {
+                NetCredential = new NetCredential() { CredentialName = "Test" }
+            };
+
+            var result = command.Invoke().GetEnumerator().MoveNext();
+
+            mockedDb.Verify(x => x.RemoveNetCredentialAsync(It.IsAny<string>()), Times.Once);
+            mockedDb.Verify(x => x.RemoveApplicationOptionAsync(It.IsAny<string>()), Times.Exactly(2));
+
+            Assert.IsNotNull(databaseSentCredentialName);
+            Assert.AreEqual("Test", databaseSentCredentialName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ItemNotFoundException))]
+        public void RemoveArchivialNetworkCredentialCommand_ThrowsIfProviderIsNotFound()
+        {
+            var mockedDb = new Mock<IClientDatabase>();
+
+            string databaseSentCredentialName = null;
+
+            mockedDb.Setup(x => x.GetNetCredentialsAsync()).ReturnsAsync(
+                new NetCredentialsCollection()
+                {
+                    new NetCredential() { CredentialName = "Test" }
+                }
+            );
+
+            mockedDb.Setup(x => x.RemoveNetCredentialAsync(It.IsAny<string>()))
+                .Returns(Task.CompletedTask)
+                .Callback<string>(x => databaseSentCredentialName = x);
+
+            var command = new RemoveArchivialNetworkCredentialCommand(mockedDb.Object)
+            {
+                CredentialName = "TestTest"
+            };
+
+            var result = command.Invoke().GetEnumerator().MoveNext();
+        }
+    }
+}
---
+++ ArchivialPowerShellTests/Functions/Public/RemoveArchivialProviderCommandTests.cs
@@ -0,0 +1,141 @@
+﻿using ArchivialLibrary.Database;
+using ArchivialLibrary.Providers;
+using ArchivialPowerShell.Functions.Public;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Management.Automation;
+using System.Threading.Tasks;
+
+namespace ArchivialPowerShellTests.Functions.Public
+{
+    [TestClass]
+    public class RemoveArchivialProviderCommandTests
+    {
+        [TestMethod]
+        public void RemoveArchivialProviderCommand_ProviderParameter_HasRequiredAttributes()
+        {
+            Assert.IsTrue(
+                TypeHelpers.CmdletParameterHasAttribute(
+                    typeof(RemoveArchivialProviderCommand),
+                    nameof(RemoveArchivialProviderCommand.Provider),
+                    typeof(ParameterAttribute))
+            );
+
+            Assert.IsTrue(
+                TypeHelpers.CmdletParameterHasAttribute(
+                    typeof(RemoveArchivialProviderCommand),
+                    nameof(RemoveArchivialProviderCommand.Provider),
+                    typeof(ValidateNotNullAttribute))
+            );
+        }
+
+        [TestMethod]
+        public void RemoveArchivialProviderCommand_ProviderNameParameter_HasRequiredAttributes()
+        {
+            Assert.IsTrue(
+                TypeHelpers.CmdletParameterHasAttribute(
+                    typeof(RemoveArchivialProviderCommand),
+                    nameof(RemoveArchivialProviderCommand.ProviderName),
+                    typeof(ParameterAttribute))
+            );
+
+            Assert.IsTrue(
+                TypeHelpers.CmdletParameterHasAttribute(
+                    typeof(RemoveArchivialProviderCommand),
+                    nameof(RemoveArchivialProviderCommand.ProviderName),
+                    typeof(ValidateNotNullOrEmptyAttribute))
+            );
+        }
+
+        [TestMethod]
+        public void RemoveArchivialProviderCommand_CanRemoveSource_FromProviderName()
+        {
+            var mockedDb = new Mock<IClientDatabase>();
+
+            string databaseSentProviderName = null;
+
+            mockedDb.Setup(x => x.GetProvidersAsync(ProviderTypes.Any)).ReturnsAsync(
+                new ProviderCollection()
+                {
+                    new Provider() { Name = "Azure" }
+                }
+            );
+
+            mockedDb.Setup(x => x.RemoveProviderAsync(It.IsAny<string>()))
+                .Returns(Task.CompletedTask)
+                .Callback<string>(x => databaseSentProviderName = x);
+
+            var command = new RemoveArchivialProviderCommand(mockedDb.Object)
+            {
+                ProviderName = "Azure"
+            };
+
+            var result = command.Invoke().GetEnumerator().MoveNext();
+
+            mockedDb.Verify(x => x.RemoveProviderAsync(It.IsAny<string>()), Times.Once);
+            mockedDb.Verify(x => x.RemoveApplicationOptionAsync(It.IsAny<string>()), Times.Exactly(2));
+
+            Assert.IsNotNull(databaseSentProviderName);
+            Assert.AreEqual("Azure", databaseSentProviderName);
+        }
+
+        [TestMethod]
+        public void RemoveArchivialProviderCommand_CanRemoveSource_FromSourceObject()
+        {
+            var mockedDb = new Mock<IClientDatabase>();
+
+            string databaseSentProviderName = null;
+
+            mockedDb.Setup(x => x.GetProvidersAsync(ProviderTypes.Any)).ReturnsAsync(
+                new ProviderCollection()
+                {
+                    new Provider() { Name = "Azure" }
+                }
+            );
+
+            mockedDb.Setup(x => x.RemoveProviderAsync(It.IsAny<string>()))
+                .Returns(Task.CompletedTask)
+                .Callback<string>(x => databaseSentProviderName = x);
+
+            var command = new RemoveArchivialProviderCommand(mockedDb.Object)
+            {
+                Provider = new Provider() { Name = "Azure" }
+            };
+
+            var result = command.Invoke().GetEnumerator().MoveNext();
+
+            mockedDb.Verify(x => x.RemoveProviderAsync(It.IsAny<string>()), Times.Once);
+            mockedDb.Verify(x => x.RemoveApplicationOptionAsync(It.IsAny<string>()), Times.Exactly(2));
+
+            Assert.IsNotNull(databaseSentProviderName);
+            Assert.AreEqual("Azure", databaseSentProviderName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ItemNotFoundException))]
+        public void RemoveArchivialProviderCommand_ThrowsIfProviderIsNotFound()
+        {
+            var mockedDb = new Mock<IClientDatabase>();
+
+            string databaseSentProviderName = null;
+
+            mockedDb.Setup(x => x.GetProvidersAsync(ProviderTypes.Any)).ReturnsAsync(
+                new ProviderCollection()
+                {
+                    new Provider() { Name = "Azure" }
+                }
+            );
+
+            mockedDb.Setup(x => x.RemoveProviderAsync(It.IsAny<string>()))
+                .Returns(Task.CompletedTask)
+                .Callback<string>(x => databaseSentProviderName = x);
+
+            var command = new RemoveArchivialProviderCommand(mockedDb.Object)
+            {
+                ProviderName = "AWS"
+            };
+
+            var result = command.Invoke().GetEnumerator().MoveNext();
+        }
+    }
+}
