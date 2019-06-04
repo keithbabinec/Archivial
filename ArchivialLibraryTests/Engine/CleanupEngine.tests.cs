43c842233a8b9c387c8b1d8174ca7481801c1af3
Keith Babinec <keith.babinec@gmail.com>
2019-06-03 20:19:12 -0700

    Added missing unit tests for CleanupEngine and CoreServiceEngine classes

---
+++ ArchivialLibraryTests/Engine/CleanupEngine.tests.cs
@@ -0,0 +1,86 @@
+﻿using ArchivialLibrary.Database.SQLServer;
+using ArchivialLibrary.Engine;
+using ArchivialLibrary.Logging.Mock;
+using ArchivialLibrary.ServiceCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading;
+
+namespace ArchivialLibraryTests.Engine
+{
+    [TestClass]
+    public class CleanupEngineTests
+    {
+        private const string TestConnectionString = "fakedb";
+
+        private ICoreSettings SharedMockedCoreSettings = new Mock<ICoreSettings>().Object;
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CleanupEngineConstructorThrowsExceptionWhenNoDatabaseIsProvided()
+        {
+            var engine = new CleanupEngine(null, new MockLogger(), 0, SharedMockedCoreSettings);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CleanupEngineConstructorThrowsExceptionWhenNoLoggerIsProvided()
+        {
+            var db = new SQLServerClientDatabase(TestConnectionString, new MockLogger(), SharedMockedCoreSettings);
+
+            var engine = new CleanupEngine(db, null, 0, SharedMockedCoreSettings);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CleanupEngineConstructorThrowsExceptionWhenNoCoreSettingsProvided()
+        {
+            var db = new SQLServerClientDatabase(TestConnectionString, new MockLogger(), SharedMockedCoreSettings);
+
+            var engine = new CleanupEngine(db, new MockLogger(), 0, null);
+        }
+
+        [TestMethod]
+        public void CleanupEngineConstructorDoesNotThrowWhenValidArgumentsAreProvided()
+        {
+            var logger = new MockLogger();
+            var db = new SQLServerClientDatabase(TestConnectionString, new MockLogger(), SharedMockedCoreSettings);
+
+            var engine = new CleanupEngine(db, logger, 0, SharedMockedCoreSettings);
+
+            Assert.IsNotNull(engine);
+        }
+
+        [TestMethod]
+        public void CleanupEngineCanStartAndStop()
+        {
+            var logger = new MockLogger();
+            var db = new SQLServerClientDatabase(TestConnectionString, new MockLogger(), SharedMockedCoreSettings);
+
+            var engine = new CleanupEngine(db, logger, 0, SharedMockedCoreSettings);
+
+            engine.BeginStart();
+            engine.BeginStop();
+        }
+
+        [TestMethod]
+        public void CleanupEngineTriggersStoppedEventWhenEngineHasStopped()
+        {
+            var logger = new MockLogger();
+            var db = new SQLServerClientDatabase(TestConnectionString, new MockLogger(), SharedMockedCoreSettings);
+
+            var engine = new CleanupEngine(db, logger, 0, SharedMockedCoreSettings);
+
+            var signalStoppedEvent = new AutoResetEvent(false);
+
+            engine.Stopped += (s, e) => { signalStoppedEvent.Set(); };
+            engine.BeginStart();
+            engine.BeginStop();
+
+            var engineStoppedSignaled = signalStoppedEvent.WaitOne(TimeSpan.FromSeconds(5));
+
+            Assert.IsTrue(engineStoppedSignaled);
+        }
+    }
+}
